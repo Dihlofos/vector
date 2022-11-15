cb4dabc04f6d7ea66a3af19bce78fd8a0becb21c
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2022-11-15 09:47:18 +0700

    done

--- source/js/main/modal.js
+++ source/js/main/modal.js
@@ -2,5 +2,23 @@
 (function () {
   const trigger = document.querySelector('.js-modal-trigger');
   const modal = document.querySelector('.js-modal');
-  const modalClose = document.querySelector('.js-modal-close');
+  const modalClose = document.querySelectorAll('.js-modal-close');
+
+  trigger.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    setTimeout(()=>{
+      modal.classList.add('is-show');
+    },100)
+
+  })
+
+  modalClose.forEach((closeElement)=>{
+    closeElement.addEventListener('click', () => {
+      modal.classList.remove('is-show');
+      setTimeout(()=>{
+        modal.style.display = 'none';
+      },300)
+
+    })
+  })
 })();
